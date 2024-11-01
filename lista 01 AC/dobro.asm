b82d9864b01945e4ffed4716771105737898de04
Antônio Irineu Filho <161607435+antchan2911@users.noreply.github.com>
2024-11-01 09:35:12 -0300

    Delete lista 01 AC directory

--- lista 01 AC/dobro.asm
+++
@@ -1,7 +0,0 @@
-.text
-main: addi $2, $0, 5
-      syscall
-      add $8, $2, $2
-      add $4, $0, $8
-      addi $2, $0, 1
-      syscall 
--- lista 01 AC/quadrado.asm
+++
@@ -1,7 +0,0 @@
-.text
-main: addi $2, $0, 5
-      syscall
-      mul $9, $2, $2
-      add $4, $0, $9
-      addi $2, $0, 1
-      syscall
