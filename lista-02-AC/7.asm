cd9686e0da05735b4f2ac9fce135fa3272149266
Antônio Irineu Filho <20241014040008@ifrn.local>
2024-11-05 10:23:28 -0300

    update

--- lista-02-AC/7.asm
+++ lista-02-AC/7.asm
@@ -14,9 +14,11 @@ main:
 	div $11, $10
 	mfhi $10 # resto da div por 400
 	or $11, $9, $10
+	addi $10, $0, 1
+	slt $12, $9, $10 # resto da div p/100 é menor que 1? se sim -> 1 : 0
 	
 	
-	beq $8, $0, bissexto
+	beq $8, $12, bissexto
 	beq $11, $0, bissexto
 	addi $4, $0, 'N'
 	j print
